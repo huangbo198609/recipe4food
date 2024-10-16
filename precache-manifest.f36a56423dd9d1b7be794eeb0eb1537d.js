6790d59e7aee351efc9b93bdba43ec3d9d5237a0
BoHuang <huangb@live.com>
2024-10-16 10:05:31 +0200

    Updates

---
+++ precache-manifest.f36a56423dd9d1b7be794eeb0eb1537d.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c2e52d99dde55f68efae2e8a43d91ee4",
+    "url": "/recipe4food/index.html"
+  },
+  {
+    "revision": "48fd494ef845481be604",
+    "url": "/recipe4food/static/css/main.435d5fe5.chunk.css"
+  },
+  {
+    "revision": "2d7978f366aa6e33683b",
+    "url": "/recipe4food/static/js/2.dc298318.chunk.js"
+  },
+  {
+    "revision": "60f6bf9e100e456690e9ab6c9a37bfc2",
+    "url": "/recipe4food/static/js/2.dc298318.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "60589a27974ae6d885d9",
+    "url": "/recipe4food/static/js/3.cf93a425.chunk.js"
+  },
+  {
+    "revision": "48fd494ef845481be604",
+    "url": "/recipe4food/static/js/main.d7f2e6a8.chunk.js"
+  },
+  {
+    "revision": "77b8b2e9c6a2db4ab484",
+    "url": "/recipe4food/static/js/runtime-main.50b5d1b9.js"
+  },
+  {
+    "revision": "06e733283fa43d1dd57738cfc409adbd",
+    "url": "/recipe4food/static/media/logo.06e73328.svg"
+  }
+]);
